44ad07bb5aab4368e30e9aa7737d7c35961cd25f
alamin-karno <amin15-1951@diu.edu.bd>
2022-10-25 15:31:26 +0600

    ADDED MORE NUMBER CHECKING OPTION

--- lib/src/check_number.dart
+++ lib/src/check_number.dart
@@ -1,6 +1,8 @@
+import 'dart:math';
+
 class FlutterNumberChecker {
   // is prime number
-  bool isPrimeNumber(num n) {
+  static bool isPrimeNumber(num n) {
     if (n < 4 && n > 1) {
       return true;
     } else {
@@ -22,5 +24,503 @@ class FlutterNumberChecker {
         return false;
       }
     }
+  }
+
+  // is even number
+  static bool isEvenNumber(num n) {
+    if (n % 2 == 0) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  // is odd number
+  static bool isOddNumber(num n) {
+    if (n % 2 == 0) {
+      return false;
+    } else {
+      return true;
+    }
+  }
+
+  // is palindrome number
+  static bool isPalindromeNumber(num n) {
+    String numString = n.toString();
+    String reverseNumString = numString.split('').reversed.join();
+
+    if (numString == reverseNumString) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  // is armstrong number
+  static bool isArmstrongNumber(num n) {
+    String numString = n.toString();
+    int numLength = numString.length;
+    int sum = 0;
+
+    for (int i = 0; i < numLength; i++) {
+      int num = int.parse(numString[i]);
+      sum += num * num * num;
+    }
+
+    if (sum == n) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  // is perfect number
+  static bool isPerfectNumber(num n) {
+    int sum = 0;
+
+    for (int i = 1; i < n; i++) {
+      if (n % i == 0) {
+        sum += i;
+      }
+    }
+
+    if (sum == n) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  // is perfect square
+  static bool isPerfectSquare(num n) {
+    double sqrtNum = sqrt(n);
+
+    if (sqrtNum == sqrtNum.roundToDouble()) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  // is perfect cube
+  static bool isPerfectCube(num n) {
+    double cubeRootNum = pow(n, 1 / 3).toDouble();
+
+    if (cubeRootNum == cubeRootNum.roundToDouble()) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  // is happy number
+  static bool isHappyNumber(num n) {
+    int sum = 0;
+    int count = 0;
+
+    while (sum != 1 && count < 10) {
+      sum = 0;
+      String numString = n.toString();
+      int numLength = numString.length;
+
+      for (int i = 0; i < numLength; i++) {
+        int num = int.parse(numString[i]);
+        sum += num * num;
+      }
+
+      n = sum;
+      count++;
+    }
+
+    if (sum == 1) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  //is sad number
+  static bool isSadNumber(num n) {
+    int sum = 0;
+    int count = 0;
+
+    while (sum != 1 && count < 10) {
+      sum = 0;
+      String numString = n.toString();
+      int numLength = numString.length;
+
+      for (int i = 0; i < numLength; i++) {
+        int num = int.parse(numString[i]);
+        sum += num * num;
+      }
+
+      n = sum;
+      count++;
+    }
+
+    if (sum != 1) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  // is fibonacci number
+  static bool isFibonacciNumber(num n) {
+    int a = 0;
+    int b = 1;
+    int c = 0;
+
+    while (c < n) {
+      c = a + b;
+      a = b;
+      b = c;
+    }
+
+    if (c == n) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  // is square number
+  static bool isSquareNumber(num n) {
+    int a = 0;
+    int b = 1;
+    int c = 0;
+
+    while (c < n) {
+      c = a + b;
+      a = b;
+      b = c;
+    }
+
+    if (c == n) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  // is triangular number
+  static bool isTriangularNumber(num n) {
+    int sum = 0;
+
+    for (int i = 1; i < n; i++) {
+      sum += i;
+
+      if (sum == n) {
+        return true;
+      }
+    }
+
+    return false;
+  }
+
+  // is cube number
+  static bool isCubeNumber(num n) {
+    int a = 0;
+    int b = 1;
+    int c = 0;
+
+    while (c < n) {
+      c = a + b;
+      a = b;
+      b = c;
+    }
+
+    if (c == n) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  // is harshad number
+  static bool isHarshadNumber(num n) {
+    String numString = n.toString();
+    int numLength = numString.length;
+    int sum = 0;
+
+    for (int i = 0; i < numLength; i++) {
+      int num = int.parse(numString[i]);
+      sum += num;
+    }
+
+    if (n % sum == 0) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  // is smith number
+  static bool isSmithNumber(num n) {
+    String numString = n.toString();
+    int numLength = numString.length;
+    int sum = 0;
+
+    for (int i = 0; i < numLength; i++) {
+      int num = int.parse(numString[i]);
+      sum += num;
+    }
+
+    int sumOfPrimeFactors = 0;
+
+    for (int i = 2; i < n; i++) {
+      if (n % i == 0) {
+        if (isPrimeNumber(i)) {
+          String primeFactorString = i.toString();
+          int primeFactorLength = primeFactorString.length;
+
+          for (int j = 0; j < primeFactorLength; j++) {
+            int primeFactor = int.parse(primeFactorString[j]);
+            sumOfPrimeFactors += primeFactor;
+          }
+        }
+      }
+    }
+
+    if (sum == sumOfPrimeFactors) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  // is duck number
+  static bool isDuckNumber(num n) {
+    String numString = n.toString();
+
+    if (numString[0] == '0') {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  // is spy number
+  static bool isSpyNumber(num n) {
+    String numString = n.toString();
+    int numLength = numString.length;
+    int sum = 0;
+    int product = 1;
+
+    for (int i = 0; i < numLength; i++) {
+      int num = int.parse(numString[i]);
+      sum += num;
+      product *= num;
+    }
+
+    if (sum == product) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  // is neon number
+  static bool isNeonNumber(num n) {
+    int square = (n * n).toInt();
+    String squareString = square.toString();
+    int squareLength = squareString.length;
+    int sum = 0;
+
+    for (int i = 0; i < squareLength; i++) {
+      int num = int.parse(squareString[i]);
+      sum += num;
+    }
+
+    if (sum == n) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  // is automorphic number
+  static bool isAutomorphicNumber(num n) {
+    int square = (n * n).toInt();
+    String squareString = square.toString();
+    String numString = n.toString();
+
+    if (squareString.endsWith(numString)) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  // is strobogrammatic number
+  static bool isStrobogrammaticNumber(num n) {
+    String numString = n.toString();
+    int numLength = numString.length;
+    String reverseString = '';
+
+    for (int i = numLength - 1; i >= 0; i--) {
+      reverseString += numString[i];
+    }
+
+    if (numString == reverseString) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  // is kaprekar number
+  static bool isKaprekarNumber(num n) {
+    int square = (n * n).toInt();
+    String squareString = square.toString();
+    int squareLength = squareString.length;
+    int sum = 0;
+
+    for (int i = 0; i < squareLength; i++) {
+      int num = int.parse(squareString[i]);
+      sum += num;
+    }
+
+    if (sum == n) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  // is disarium number
+  static bool isDisariumNumber(num n) {
+    String numString = n.toString();
+    int numLength = numString.length;
+    int sum = 0;
+
+    for (int i = 0; i < numLength; i++) {
+      int num = int.parse(numString[i]);
+      sum += pow(num, i + 1).toInt();
+    }
+
+    if (sum == n) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  // is krishnamurthy number
+  static bool isKrishnamurthyNumber(num n) {
+    String numString = n.toString();
+    int numLength = numString.length;
+    int sum = 0;
+
+    for (int i = 0; i < numLength; i++) {
+      int num = int.parse(numString[i]);
+      sum += factorial(num);
+    }
+
+    if (sum == n) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  static int factorial(int n) {
+    return n == 1 ? 1 : n * factorial(n - 1);
+  }
+
+  // is emirp number
+  static bool isEmirpNumber(num n) {
+    if (isPrimeNumber(n)) {
+      String numString = n.toString();
+      int numLength = numString.length;
+      String reverseString = '';
+
+      for (int i = numLength - 1; i >= 0; i--) {
+        reverseString += numString[i];
+      }
+
+      int reverseNumber = int.parse(reverseString);
+
+      if (isPrimeNumber(reverseNumber)) {
+        return true;
+      } else {
+        return false;
+      }
+    } else {
+      return false;
+    }
+  }
+
+  // is abundant number
+  static bool isAbundantNumber(num n) {
+    int sum = 0;
+
+    for (int i = 1; i < n; i++) {
+      if (n % i == 0) {
+        sum += i;
+      }
+    }
+
+    if (sum > n) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  // is deficient number
+  static bool isDeficientNumber(num n) {
+    int sum = 0;
+
+    for (int i = 1; i < n; i++) {
+      if (n % i == 0) {
+        sum += i;
+      }
+    }
+
+    if (sum < n) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  // is factorial number
+  static bool isFactorialNumber(num n) {
+    int sum = 0;
+
+    for (int i = 1; i < n; i++) {
+      sum += factorial(i);
+
+      if (sum == n) {
+        return true;
+      }
+    }
+
+    return false;
+  }
+
+  // is circular prime number
+  static bool isCircularPrimeNumber(num n) {
+    String numString = n.toString();
+    int numLength = numString.length;
+    String circularPrimeString = '';
+
+    for (int i = 0; i < numLength; i++) {
+      circularPrimeString += numString[i];
+    }
+
+    for (int i = 0; i < numLength; i++) {
+      int circularPrimeNumber = int.parse(circularPrimeString);
+
+      if (!isPrimeNumber(circularPrimeNumber)) {
+        return false;
+      }
+
+      circularPrimeString = circularPrimeString.substring(1) +
+          circularPrimeString.substring(0, 1);
+    }
+
+    return true;
   }
 }
--- lib/src/check_number_extention.dart
+++ lib/src/check_number_extention.dart
@@ -1,28 +1,94 @@
+import 'package:flutter_number_checker/flutter_number_checker.dart';
+
 extension CheckNumberExtention on num {
   // [Check number is prime or not]
-  bool get isPrimeNumber => _isPrimeNumber(this);
+  bool get isPrimeNumber => FlutterNumberChecker.isPrimeNumber(this);
 
-  bool _isPrimeNumber(num n) {
-    if (n < 4 && n > 1) {
-      return true;
-    } else {
-      bool primeFlag = true;
-      int halfOfNum = n ~/ 2;
+  // [Check number is even or not]
+  bool get isEvenNumber => FlutterNumberChecker.isEvenNumber(this);
 
-      for (int primeIterator = 2; primeIterator < halfOfNum; primeIterator++) {
-        if (n % primeIterator == 0) {
-          primeFlag = false;
-          break;
-        } else {
-          primeFlag = true;
-        }
-      }
+  // [Check number is odd or not]
+  bool get isOddNumber => FlutterNumberChecker.isOddNumber(this);
 
-      if (primeFlag) {
-        return true;
-      } else {
-        return false;
-      }
-    }
-  }
+  // [Check number is palindrome or not]
+  bool get isPalindromeNumber => FlutterNumberChecker.isPalindromeNumber(this);
+
+  // [Check number is armstrong or not]
+  bool get isArmstrongNumber => FlutterNumberChecker.isArmstrongNumber(this);
+
+  // [Check number is perfect square or not]
+  bool get isPerfectSquare => FlutterNumberChecker.isPerfectSquare(this);
+
+  // [Check number is perfect number or not]
+  bool get isPerfectNumber => FlutterNumberChecker.isPerfectNumber(this);
+
+  // [Check number is perfect cube or not]
+  bool get isPerfectCube => FlutterNumberChecker.isPerfectCube(this);
+
+  // [Check number is happy number or not]
+  bool get isHappyNumber => FlutterNumberChecker.isHappyNumber(this);
+
+  // [Check number is sad number or not]
+  bool get isSadNumber => FlutterNumberChecker.isSadNumber(this);
+
+  // [Check number is square number or not]
+  bool get isSquareNumber => FlutterNumberChecker.isSquareNumber(this);
+
+  // [Check number is triangular number or not]
+  bool get isTriangularNumber => FlutterNumberChecker.isTriangularNumber(this);
+
+  // [Check number is cube number or not]
+  bool get isCubeNumber => FlutterNumberChecker.isCubeNumber(this);
+
+  // [Check number is harshad number or not]
+  bool get isHarshadNumber => FlutterNumberChecker.isHarshadNumber(this);
+
+  // [Check number is smith number or not]
+  bool get isSmithNumber => FlutterNumberChecker.isSmithNumber(this);
+
+  // [Check number is duck number or not]
+  bool get isDuckNumber => FlutterNumberChecker.isDuckNumber(this);
+
+  // [Check number is spy number or not]
+  bool get isSpyNumber => FlutterNumberChecker.isSpyNumber(this);
+
+  // [Check number is neon number or not]
+  bool get isNeonNumber => FlutterNumberChecker.isNeonNumber(this);
+
+  // [Check number is automorphic number or not]
+  bool get isAutomorphicNumber =>
+      FlutterNumberChecker.isAutomorphicNumber(this);
+
+  // [Check number is strobogrammatic number or not]
+  bool get isStrobogrammaticNumber =>
+      FlutterNumberChecker.isStrobogrammaticNumber(this);
+
+  // [Check number is disarium number or not]
+  bool get isDisariumNumber => FlutterNumberChecker.isDisariumNumber(this);
+
+  // [Check number is kaprekar number or not]
+  bool get isKaprekarNumber => FlutterNumberChecker.isKaprekarNumber(this);
+
+  // [Check number is krishnamurthy number or not]
+  bool get isKrishnamurthyNumber =>
+      FlutterNumberChecker.isKrishnamurthyNumber(this);
+
+  // [Check number is emirp number or not]
+  bool get isEmirpNumber => FlutterNumberChecker.isEmirpNumber(this);
+
+  // [Check number is abundant number or not]
+  bool get isAbundantNumber => FlutterNumberChecker.isAbundantNumber(this);
+
+  // [Check number is deficient number or not]
+  bool get isDeficientNumber => FlutterNumberChecker.isDeficientNumber(this);
+
+  // [Check number is fibonacci number or not]
+  bool get isFibonaciNumber => FlutterNumberChecker.isFibonacciNumber(this);
+
+  // [Check number is factorial number or not]
+  bool get isFactorialNumber => FlutterNumberChecker.isFactorialNumber(this);
+
+  // [Check number is circular prime number or not]
+  bool get isCircularPrimeNumber =>
+      FlutterNumberChecker.isCircularPrimeNumber(this);
 }
